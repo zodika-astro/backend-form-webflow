7f2321ca6ba86c325895af083b8e3ad57983d831
Isadora Rocha <info@zodika.com.br>
2025-08-06 12:46:29 +0200

    Update db.js

--- db.js
+++ db.js
@@ -1,12 +1,14 @@
 const { Pool } = require('pg');
 
+// Configuração da conexão com o banco de dados PostgreSQL do Railway
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL, // configure no Railway
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
 });
 
+// Exporta a função de consulta para ser usada em outros arquivos
 module.exports = {
   query: (text, params) => pool.query(text, params)
 };
