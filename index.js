c5cd355ccd84547ce51f8a221a7cfd3637648f38
Isadora Rocha <info@zodika.com.br>
2025-08-07 21:16:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,20 @@ app.get('/health', (req, res) => res.status(200).send('OK'));
 // Rote birth-chart
 app.use('/birth-chartendpoint', birthchartRouter);
 
+// index.js (provisório)
+app.get('/dev/set-sequence-birthchart', async (req, res) => {
+  try {
+    await db.query(`
+      ALTER SEQUENCE birthchart_request_id_seq RESTART WITH 1001;
+    `);
+    res.send('Sequência do ID atualizada para começar do 1001');
+  } catch (error) {
+    console.error('Erro ao atualizar sequência:', error);
+    res.status(500).send('Erro ao atualizar sequência: ' + error.message);
+  }
+});
+
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
