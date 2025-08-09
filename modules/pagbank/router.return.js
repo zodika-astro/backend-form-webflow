1963b385a696114383c7d803354e6646813313eb
Isadora Rocha <info@zodika.com.br>
2025-08-09 16:24:22 +0200

    Delete modules/pagbank directory

--- modules/pagbank/router.return.js
+++
@@ -1,8 +0,0 @@
-// modules/pagbank/router.return.js
-const express = require('express');
-const router = express.Router();
-const pagbankController = require('./controller');
-
-router.get('/callbacks/pagbank/return', pagbankController.handleReturn);
-
-module.exports = router;
