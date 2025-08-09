4d3e42bde7cb02ea657d9ba5854463eae7d5f5cb
Isadora Rocha <info@zodika.com.br>
2025-08-09 19:41:02 +0200

    Delete routes directory

--- routes/birthchart.route.js
+++
@@ -1,9 +0,0 @@
-// routes/birthchart.route.js
-const express = require('express');
-const router = express.Router();
-const birthchartController = require('../controllers/birthchart.controller');
-
-// POST /birth-chartendpoint
-router.post('/', birthchartController.BirthChartRequest);
-
-module.exports = router;
