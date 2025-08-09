3d61a965b0dfdb99cabd6e739f5cc0e11b9f6349
Isadora Rocha <info@zodika.com.br>
2025-08-09 20:10:19 +0200

    Update env.js

--- config/env.js
+++ config/env.js
@@ -6,8 +6,8 @@ const { cleanEnv, url, str, num } = envalid;
 
 dotenv.config();
 
-export const env = cleanEnv(process.env, {
+module.exports = cleanEnv(process.env, {
   DATABASE_URL: url({ desc: 'Postgres connection string' }),
-  PAGBANK_TOKEN: str({ desc: 'PagBank authentication token' }),
-  WEBFLOW_DOMAIN: url({ desc: 'Webflow site domain for referer validation' })
+  PAGBANK_API_TOKEN: str({ desc: 'PagBank API authentication token' }),
+  WEBFLOW_DOMAIN: url({ desc: 'Webflow site domain for referer validation' }),
 });
