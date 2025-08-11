759c2a67c205ed986a955b6df3a1d83bb757c877
Isadora Rocha <info@zodika.com.br>
2025-08-11 23:01:14 +0200

    Update refererAuth.js

--- middlewares/refererAuth.js
+++ middlewares/refererAuth.js
@@ -1,14 +1,13 @@
 // middlewares/refererAuth.js
-const ALLOWED_REFERER = process.env.WEBFLOW_DOMAIN; 
+
+const ALLOWED_REFERER = 'https://www.zodika.com.br/';
 
 function refererAuth(req, res, next) {
     const referer = req.headers.referer;
-
     
     if (!referer || !referer.startsWith(ALLOWED_REFERER)) {
         return res.status(403).json({ message: 'Forbidden: Invalid referer' });
-    }
-
+}
     next();
 }
 
