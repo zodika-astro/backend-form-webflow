899805ae065a80a3a9682c2279f613ebe429e51e
Isadora Rocha <info@zodika.com.br>
2025-08-11 14:15:50 +0200

    Create logger.js

---
+++ utils/logger.js
@@ -0,0 +1,9 @@
+// utils/logger.js
+
+const logger = {
+  info: (...args) => console.log('[INFO]', ...args),
+  warn: (...args) => console.warn('[WARN]', ...args),
+  error: (...args) => console.error('[ERROR]', ...args),
+};
+
+module.exports = logger;
