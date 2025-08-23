823bbb7a0c54f973adc952b8130dbb1a86532cb5
Isadora Rocha <info@zodika.com.br>
2025-08-23 20:55:00 +0200

    Update router.return.js

--- payments/pagBank/router.return.js
+++ payments/pagBank/router.return.js
@@ -4,6 +4,6 @@ const express = require('express');
 const router = express.Router();
 const pagbankController = require('./controller');
 
-router.get('/pagbank/return', pagbankController.handleReturn);
+router.get('/return', pagbankController.handleReturn);
 
 module.exports = router;
