319b97b6adcc89338419834892b82ea00d32947a
Isadora Rocha <info@zodika.com.br>
2025-08-23 16:27:21 +0200

    Update env.js

--- config/env.js
+++ config/env.js
@@ -9,6 +9,8 @@ const env = cleanEnv(process.env, {
   DATABASE_URL: url({ desc: 'Postgres connection string' }),
   PAGBANK_API_TOKEN: str({ desc: 'PagBank API authentication token' }),
   PAGBANK_BASE_URL: url({ desc: 'PagBank URL' }),
+  ALLOWED_REFERERS: str({ desc: ''  }),
+  ALLOWED_ORIGINS:  str({ desc: ''  }),
 });
 
-module.exports = { env }; // ✅ agora { env } funciona
+module.exports = { env };
