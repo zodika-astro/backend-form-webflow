149416cca51be8c2168a93560af41419fd493c71
Isadora Rocha <info@zodika.com.br>
2025-08-28 21:04:53 +0200

    Update router.webhook.js

--- payments/pagBank/router.webhook.js
+++ payments/pagBank/router.webhook.js
@@ -5,9 +5,10 @@ const router = express.Router();
 const pagbankWebhookAuth = require('../../middlewares/pagbankWebhookAuth');
 const pagbankController = require('./controller');
 
-const secret = process.env.WEBHOOK_PATH_SECRET || ''; 
+const secret = process.env.WEBHOOK_PATH_SECRET || '';
 const path = secret ? `/webhook/pagbank/${secret}` : '/webhook/pagbank';
 
+router.get(path, (req, res) => res.status(200).send('OK'));
 router.post(path, pagbankWebhookAuth, pagbankController.handleWebhook);
 
 module.exports = router;
