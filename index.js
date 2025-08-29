ccf4092b9383bf2c84e5cd08163ecb2d56ae727b
Isadora Rocha <info@zodika.com.br>
2025-08-29 09:33:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,55 +1,47 @@
 // index.js
-// Carrega variáveis de ambiente e configura o pool do banco de dados antes de tudo.
+
 require('./config/env'); // valida ENV com envalid
 require('./db/db');      // inicializa pool do PG
 
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const path = require('path');
 
-// Middlewares centrais
+// Middlewares
 const corsMiddleware = require('./middlewares/cors');
 const errorHandlerMiddleware = require('./middlewares/errorHandler');
 
-// Routers e controllers
+// Routers and controllers
 const birthchartRouter = require('./modules/birthchart/router');
 const pagbankWebhookRouter = require('./payments/pagBank/router.webhook');
 const pagbankReturnRouter  = require('./payments/pagBank/router.return.js');
 const pagbankController    = require('./payments/pagBank/controller');
 
-// Middleware globais
+// Global Middleware 
 app.use(corsMiddleware);
 app.use(express.json({
   verify: (req, res, buf) => { req.rawBody = buf.toString('utf8'); }
 }));
 
-
 // Health check
 app.get('/health', (req, res) => res.status(200).send('OK'));
 
-// ======================
-// Módulo Birthchart
-// ======================
+// public
+app.use('/assets', express.static(path.join(__dirname, 'public'), { maxAge: '30d', etag: true }));
+
+// product modules
 app.use('/birthchart', birthchartRouter);
 
-// ======================
-// Módulo PagBank
-// ======================
-// Padrão do projeto em /pagBank
+// payments modules
 app.use('/pagBank', pagbankWebhookRouter);
 app.use('/pagBank', pagbankReturnRouter);
+app.use('/', pagbankWebhookRouter);
 
-// ---- Compatibilidade de rotas ----
-// Mantém o endpoint público já configurado no PagBank:
-app.use('/', pagbankWebhookRouter); // serve POST /webhook/pagbank
-
-// Garante o retorno em camel case exatamente como desejado:
 app.get('/pagBank/return', pagbankController.handleReturn);
 
-// Handler de erros (por último)
 app.use(errorHandlerMiddleware);
 
-// Start
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
