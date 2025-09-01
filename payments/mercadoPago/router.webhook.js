727c0434da2f45e781d6c8d46d8f5ae7a0a23658
Isadora Rocha <info@zodika.com.br>
2025-09-01 11:58:40 +0200

    Update router.webhook.js

--- payments/mercadoPago/router.webhook.js
+++ payments/mercadoPago/router.webhook.js
@@ -6,8 +6,7 @@ const mpWebhookAuth = require('../../middlewares/mpWebhookAuth');
 const mpController = require('./controller');
 
 const secret = process.env.WEBHOOK_PATH_SECRET || '';
-const path = secret ? `/webhook/mercadopago/${secret}` : '/webhook/mercadopago`;
-
+const path = secret ? `/webhook/mercadopago/${secret}` : '/webhook/mercadopago';
 router.get(path, (req, res) => res.status(200).send('OK'));
 router.post(path, mpWebhookAuth, mpController.handleWebhook);
 
